6246111b31020d775b6627a3b5bcbc1b65b456d7
WardenCode <diegojeanluck@hotmail.com>
2022-04-25 13:37:49 -0500

    Feat mandatory task #6 and works

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print("Welcome to {:s} {:s}!".format(str1, str2))
-
+print("Welcome to {:s}".format(str1), end = '')
+print(" {:s}!".format(str2))
