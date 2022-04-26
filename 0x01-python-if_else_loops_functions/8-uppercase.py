2e176c4f47a20f20f73524853048739e7e1bfab2
WardenCode <diegojeanluck@hotmail.com>
2022-04-26 15:09:31 -0500

    Feat mandatory task #8 and works

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        ascii_num = ord(char)
+        if ascii_num >= 97 and ascii_num <= 122:
+            print(f"{chr(ascii_num - 32)}", end="")
+        else:
+            print(f"{char}", end="")
+    print("")
