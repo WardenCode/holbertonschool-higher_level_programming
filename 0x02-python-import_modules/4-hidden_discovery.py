b35c4ba669a17fb525f9bd9f28b646b6ea21eaae
WardenCode <diegojeanluck@hotmail.com>
2022-04-28 14:13:35 -0500

    Feat mandatory task #4 and works

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+from hidden_4 import my_secret_santa, print_school, print_hidden
+
+if __name__ == "__main__":
+    print(f"{my_secret_santa.__name__}")
+    print(f"{print_hidden.__name__}")
+    print(f"{print_school.__name__}")
