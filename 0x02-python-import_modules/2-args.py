0a29d16a4440714dc7b2afb9dedd3d7052712155
WardenCode <diegojeanluck@hotmail.com>
2022-04-28 13:16:28 -0500

    Fix typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ from sys import argv
 if __name__ == "__main__":
     i = 1
     arguments = len(argv) - 1
-    
-    print(f"{arguments} arguments{'.' if arguments == 0 else ':'}")
+
+    print(f"{'argument' if argument == 1 else 'arguments'} arguments{'.' if arguments == 0 else ':'}")
     while (i <= arguments):
         print(f"{i}: {argv[i]}")
         i += 1
