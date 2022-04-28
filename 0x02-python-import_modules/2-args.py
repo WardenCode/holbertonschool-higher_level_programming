b26f7f85528e20eaa1852fedb3c43998cffcb5ca
WardenCode <diegojeanluck@hotmail.com>
2022-04-28 13:05:26 -0500

    Feat mandatory task #2 and works

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    i = 1
+    arguments = len(argv) - 1
+    
+    print(f"{arguments} arguments{'.' if arguments == 0 else ':'}")
+    while (i <= arguments):
+        print(f"{i}: {argv[i]}")
+        i += 1
