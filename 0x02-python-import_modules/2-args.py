a51c43f377f284d9573fd21727bb5ce2c592fe7f
WardenCode <diegojeanluck@hotmail.com>
2022-04-28 13:19:26 -0500

    Fix typo when call arguments to validate

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     i = 1
     arguments = len(argv) - 1
 
-    print(f"{'argument' if argument == 1 else 'arguments'} arguments{'.' if arguments == 0 else ':'}")
+    print(f"{arguments} {'argument' if arguments == 1 else 'arguments'}{'.' if arguments == 0 else ':'}")
     while (i <= arguments):
         print(f"{i}: {argv[i]}")
         i += 1
