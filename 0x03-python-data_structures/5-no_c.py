268215aae8e0b3b9a3edbfa55276affa89670924
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 14:31:30 -0500

    Fix now have the expected behavior deletes the c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     new_str = ""
     for i in range(len(my_string)):
-        if (my_string[i] == "c"):
-            new_str += "C"
+        if (my_string[i] == "c" or my_string[i] == "C"):
+            new_str += " "
         else:
             new_str += my_string[i]
     return (new_str)
