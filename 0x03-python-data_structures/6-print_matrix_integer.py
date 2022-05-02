3451c9c62351a6d2caea483f1b4b7162a0dcd216
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 14:56:56 -0500

    Feat mandatoty task #6 and works

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            if (element == row[-1]):
+                print(element)
+            else:
+                print(element, end=" ")
