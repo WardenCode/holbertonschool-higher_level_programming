230353b87ca9f8623efd480b60cae7c6c24eb9ac
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 14:30:28 -0500

    Feat mandatory task #5 and works

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
+    for i in range(len(my_string)):
+        if (my_string[i] == "c"):
+            new_str += "C"
+        else:
+            new_str += my_string[i]
+    return (new_str)
