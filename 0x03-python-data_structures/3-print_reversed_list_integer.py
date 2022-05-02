898865c00b7fba3e21d2d91ad69827e8de2ea124
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 13:33:44 -0500

    Now doesn't modify the array

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for elem in my_list:
-        print(elem)
+    reverse_len = len(my_list) + 1
+    for elem in range(1, reverse_len):
+        print(my_list[-elem])
