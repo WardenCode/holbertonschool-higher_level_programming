b9f7c5c36854a157fc9efc21b6380a67aaadd3a3
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 13:37:02 -0500

    Using format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     reverse_len = len(my_list) + 1
     for elem in range(1, reverse_len):
-        print(my_list[-elem])
+        print("{:d}".format(my_list[-elem]))
