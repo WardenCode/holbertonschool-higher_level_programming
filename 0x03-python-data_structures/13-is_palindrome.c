66b21fc435704c239ceba3a7fba12524529bc7b1
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 23:26:27 -0500

    Feat mandatory task #13 and works

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Validate if a linked list is palindrome
+ *
+ * @head: Double pointer to a linked list.
+ *
+ * Return: 1 if the linked list is palindrome, 0 otherwise
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int res = 1, size = 0, i = 0, j = 0;
+	listint_t *node = NULL;
+	char *new_string = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (res);
+
+	for (node = *head; node; node = node->next)
+		size++;
+
+	new_string = malloc(sizeof(char) * size);
+
+	for (node = *head; node; node = node->next, i++)
+		new_string[i] = node->n;
+
+	for (i = 0, j = size; new_string[i]; i++, j--)
+	{
+		if (new_string[i] > new_string[j])
+			break;
+		if (new_string[i] != new_string[j])
+			return (0);
+	}
+
+	return (res);
+}
