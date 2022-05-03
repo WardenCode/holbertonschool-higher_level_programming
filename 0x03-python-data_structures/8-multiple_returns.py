b36263bda3c632a2bf8ebd8ea5209ecb287e1691
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 21:47:14 -0500

    Feat mandatory task #8 and works

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    size = len(sentence)
+
+    if (size != 0):
+        first_letter = sentence[0]
+
+    new_tuple = (size, first_letter) if size > 0 else (size, None)
+
+    return (new_tuple)
