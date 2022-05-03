a39f7601fd8b2302aae687603be52f5aa387d857
WardenCode <diegojeanluck@hotmail.com>
2022-05-02 22:18:17 -0500

    Feat mandatory task #10 and works

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for num in my_list:
+        res = -(-num % 2) if num < 0 else num % 2
+        new_list.append(True if res == 0 else False)
+
+    return (new_list)
