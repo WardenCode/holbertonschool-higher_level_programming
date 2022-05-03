d787e7d73e65c857eaf4c4d1ffd03a4a9f9c9c07
WardenCode <diegojeanluck@hotmail.com>
2022-05-03 01:12:49 -0500

    Using recursion

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,26 +1,39 @@
 #include "lists.h"
+#include <stdio.h>
+
+listint_t *validate_palindrome(listint_t *head, listint_t *node)
+{
+	if (node->next)
+		head = validate_palindrome(head, node->next);
+
+	if (!head || head->n != node->n)
+		return (NULL);
+
+	if (head->next)
+		head = head->next;
+
+	return (head);
+}
 
 /**
  * is_palindrome - Validate if a linked list is palindrome
  *
  * @head: Double pointer to a linked list.
  *
- * Return: 1 if the linked list is palindrome, 0 otherwise
+ * Return: 1 if the linked list is palindrome, 0 otherwise.
  */
 
 int is_palindrome(listint_t **head)
 {
-	int res = 0, counter = 0;
 	listint_t *node = NULL;
 
 	if (head == NULL || *head == NULL)
-		return (res);
+		return (1);
 
-	for (node = *head; node; node = node->next)
-		counter += node->n;
+	node = *head;
 
-	if (counter % 2 == 0)
-		res = 1;
+	if (validate_palindrome(*head, node) == NULL)
+		return (0);
 
-	return (res);
+	return (1);
 }
