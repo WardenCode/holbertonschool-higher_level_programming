a3341c1957f6d97fc3147a361bbd582b4ba404e8
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 14:08:40 -0500

    Feat mandatory task #4 and works

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_set = set()
+    for elem in set_1:
+        if elem not in set_2:
+            new_set.add(elem)
+    return (new_set)
