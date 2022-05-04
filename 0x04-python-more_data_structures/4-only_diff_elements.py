b405f51b5593def91344c30bc735f707e1bc3a8c
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 14:30:36 -0500

    Using set operations

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,21 +1,3 @@
 #!/usr/bin/python3
-def df(first_group, second_group):
-    new_set = set()
-    for elem in first_group:
-        if elem not in second_group:
-            new_set.add(elem)
-    return (new_set)
-
-
 def only_diff_elements(set_1, set_2):
-    len_1 = len(set_1)
-    len_2 = len(set_2)
-
-    if len_1 == 0 and len_2 == 0:
-        return (set())
-    elif len_1 == 0:
-        return (set_2)
-    elif len_2 == 0:
-        return (set_1)
-
-    return (df(set_1, set_2) if len_1 >= len_2 else df(set_2, set_1))
+    return (set_1 ^ set_2)
