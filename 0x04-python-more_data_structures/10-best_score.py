f3b6f262b95d935b3f47f119893f7b0bc5901163
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 15:20:10 -0500

    Feat mandatory task #10 and works

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return (None)
+    holder = ""
+    num = 0
+
+    for (key, value) in (a_dictionary.items()):
+        if value > num:
+            holder = key
+            num = value
+    return (holder)
