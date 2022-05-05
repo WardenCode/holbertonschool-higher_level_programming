65e6c7466ce7fc085138cce12c5ef4a636a579e6
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 20:20:35 -0500

    Feat mandatory task #12 and works

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+def is_roman(string, romman_letters):
+    for letter in string:
+        if letter not in romman_letters:
+            return (False)
+    return (True)
+
+
+def convert(string, roman_letters):
+    flag, counter, result = 0, 0, 0
+    size = len(string)
+
+    for digit in string:
+        if (flag):
+            flag = 0
+            counter += 1
+            continue
+
+        if (counter + 1 != size or counter == size):
+            if (string[counter] == 'I' and string[counter + 1] == 'V'):
+                result += 4
+                flag = 1
+            elif (string[counter] == 'I' and string[counter + 1] == 'X'):
+                result += 9
+                flag = 1
+            elif (string[counter] == 'X' and string[counter + 1] == 'L'):
+                result += 40
+                flag = 1
+            elif (string[counter] == 'X' and string[counter + 1] == 'C'):
+                result += 90
+                flag = 1
+            elif (string[counter] == 'C' and string[counter + 1] == 'D'):
+                result += 400
+                flag = 1
+            elif (string[counter] == 'C' and string[counter + 1] == 'M'):
+                result += 900
+                flag = 1
+            else:
+                result += roman_letters[digit]
+                counter += 1
+        else:
+            result += roman_letters[digit]
+            counter += 1
+    return (result)
+
+
+def roman_to_int(roman_string):
+    roman_letters = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    if (roman_string is None):
+        return (0)
+
+    if (is_roman(roman_string, roman_letters)):
+        return (convert(roman_string, roman_letters))
+    return (None)
