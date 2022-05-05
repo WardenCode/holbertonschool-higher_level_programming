86a7f462ffa65bd476bbe9b1ca978c78e15d3a68
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 21:24:08 -0500

    Fix when the actual value is reather than the before

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or len(roman_string) == 0:
+    if roman_string is None or type(roman_string) != str:
         return (0)
 
     digits = {'I': 1, 'V': 5, 'X': 10,
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
 
     for i in range(len(roman_string)):
         if (i > 0) and (digits[roman_string[i]] > digits[roman_string[i - 1]]):
-            result += digits[roman_string[i] - 2 * roman_string[i - 1]]
+            result += digits[roman_string[i]] - 2 * digits[roman_string[i - 1]]
         else:
             result += digits[roman_string[i]]
 
