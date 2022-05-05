7bb501c4b621329e369002353b6c21adcf4ac2ba
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 22:24:28 -0500

    Feat advanced task #102 and works

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    entries = a_dictionary.items()
+    to_elimitate = []
+
+    for actual_key, actual_value in entries:
+        if value == actual_value:
+            to_elimitate.append(actual_key)
+
+    for key in to_elimitate:
+        del a_dictionary[key]
+
+    return (a_dictionary)
