f4e0c4fa1634727ce847cfcfa77a16b44db390df
WardenCode <diegojeanluck@hotmail.com>
2022-05-04 21:43:06 -0500

    Feat advanced task #100 and works

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == []:
+        return (0)
+
+    cuantity = 0
+    sum_grades = 0
+    for grade in my_list:
+        cuantity += grade[1]
+        sum_grades += grade[0] * grade[1]
+
+    return (sum_grades / cuantity)
