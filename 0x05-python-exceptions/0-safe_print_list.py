65886e6372748f1bbeb746c327dce9bbce470ba0
WardenCode <diegojeanluck@hotmail.com>
2022-05-16 13:24:05 -0500

    Using the right way of except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for idx in range(x):
         try:
             print(f'{my_list[idx]}', end='')
-        except (Exception):
+        except (IndexError):
             continue
         counter += 1
     print()
