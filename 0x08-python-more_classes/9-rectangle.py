5f1e04fbdd99090253be69843bc7e8fd21447cc0
WardenCode <diegojeanluck@hotmail.com>
2022-05-23 19:27:37 -0500

    Put good the order in the constructor

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -23,8 +23,8 @@ class Rectangle():
             - width (default = 0): int
             - heigth (default = 0): int
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def area(self):
