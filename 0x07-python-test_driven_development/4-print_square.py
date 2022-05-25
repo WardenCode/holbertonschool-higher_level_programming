581a6e5c8c135c62e1b6cdb47fc36ffe32730c16
WardenCode <diegojeanluck@hotmail.com>
2022-05-25 00:22:54 -0500

    Fix zero case now doesn't print a newline

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,9 +15,7 @@ def print_square(size):
     if size < 0:
         raise ValueError('size must be >= 0')
 
-    if size == 0:
-        print()
-    else:
+    if size != 0:
         i = 0
         while i < size:
             print("#" * size)
