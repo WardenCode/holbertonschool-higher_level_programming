9020d80ac7ddc6c4dc28d9393874238f728e14d6
WardenCode <diegojeanluck@hotmail.com>
2022-05-30 21:59:09 -0500

    Feat mandatory task #10 and works

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+This program create a Square from a Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Class Squared based from Rectangle"""
+
+    def __init__(self, size):
+        """Constructor of Square"""
+        super().__init__(size, size)
