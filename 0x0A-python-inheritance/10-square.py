4c5ba6d0beafb06552757f16be8c41f3e71f464b
WardenCode <diegojeanluck@hotmail.com>
2022-05-30 22:05:00 -0500

    Fix pycodestyle and adding __size

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,9 +6,11 @@ This program create a Square from a Rectangle
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Class Squared based from Rectangle"""
 
     def __init__(self, size):
         """Constructor of Square"""
         super().__init__(size, size)
+        self.__size = size
