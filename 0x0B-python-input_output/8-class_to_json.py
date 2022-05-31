dd294664d68ffb01c7e42f5b5779598c182dee9a
WardenCode <diegojeanluck@hotmail.com>
2022-05-31 18:08:20 -0500

    Feat mandatory task #8 and works

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This program return the dict representation of a instance of Class.
+"""
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    Args:
+      - obj: instance of class
+    """
+
+    return (obj.__dict__)
