7698e415bf32048443bafcb4d2103ef448173874
WardenCode <diegojeanluck@hotmail.com>
2022-05-30 19:56:52 -0500

    Feat mandatory task #6 and works

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This program have an incompleted class
+"""
+
+
+class BaseGeometry():
+    """
+    This class contain a method not implemented
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
