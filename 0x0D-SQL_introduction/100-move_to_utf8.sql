e898499ab600309b9cb2bb255d5d36932eeb4f9b
WardenCode <diegojeanluck@hotmail.com>
2022-07-05 15:35:04 -0500

    Fix: add COLLATE to column

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,4 +7,4 @@ ALTER TABLE hbtn_0c_0.first_table
 	CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
-	MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+	MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
