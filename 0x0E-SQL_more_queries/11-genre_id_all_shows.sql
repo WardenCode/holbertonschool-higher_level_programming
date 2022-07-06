26283d623ac16e4f56c3bfec5f375f5b9f496ac4
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 17:20:56 -0500

    Feat: mandatory task #11 and works

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,6 @@
--- 
+-- Lists all shows contained in the database hbtn_0d_tvshows.
 
-
+SELECT s.title, s_g.genre_id FROM tv_shows AS s
+	LEFT JOIN tv_show_genres AS s_g
+	ON s.id = s_g.show_id
+	ORDER BY s.title ASC, s_g.genre_id ASC;
