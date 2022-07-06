30702a40008442c491ecfc39e61bde9d0aa4cf0b
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 18:57:19 -0500

    Feat: mandatory task #15 and works

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,9 @@
--- 
+-- Lists all Comedy shows in the database hbtn_0d_tvshows.
 
-
+SELECT s.title FROM tv_genres AS g
+	INNER JOIN tv_show_genres AS s_g
+		ON g.id = s_g.genre_id
+	INNER JOIN tv_shows AS s
+		ON s_g.show_id = s.id
+	WHERE g.name = "Comedy"
+	ORDER BY s.title ASC;
