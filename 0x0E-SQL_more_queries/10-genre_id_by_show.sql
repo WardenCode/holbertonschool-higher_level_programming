a1802e304c0ec755b65bc038b566d340764505ac
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 17:14:14 -0500

    Feat: mandatory task #10 and works

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,6 @@
--- 
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-
+SELECT s.title, s_g.genre_id FROM tv_shows AS s
+	INNER JOIN tv_show_genres AS s_g
+	ON s.id = s_g.show_id
+	ORDER BY s.title ASC, s_g.genre_id ASC;
