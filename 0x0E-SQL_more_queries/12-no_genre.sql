cd7d1576c12bfec756381e20c5959613a1cf4f5a
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 17:28:25 -0500

    Feat: mandatory task #12 and works

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,8 @@
--- 
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked.
 
+SELECT s.title, s_g.genre_id FROM tv_shows AS s
+	LEFT JOIN tv_show_genres AS s_g
+	ON s.id = s_g.show_id
+	WHERE s_g.show_id IS NULL
+	ORDER BY s.title ASC, s_g.genre_id ASC;
 
