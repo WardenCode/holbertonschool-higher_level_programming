a9f99d33d917aeea63d8dceb4e469302f3624690
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 20:08:47 -0500

    Feat: advanced task #102 and works

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,3 +1,7 @@
--- 
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
 
-
+SELECT s.title, SUM(sr.rate) AS rating FROM tv_shows AS s
+	INNER JOIN tv_show_ratings AS sr
+		ON s.id = sr.show_id
+	GROUP BY s.title
+	ORDER BY rating DESC;
