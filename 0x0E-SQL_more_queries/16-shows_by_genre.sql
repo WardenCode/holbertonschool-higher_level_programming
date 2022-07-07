9254e4a6ed9d42def81aca5cd39ba9b85e9d2bd9
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 19:10:54 -0500

    Feat: mandatory task #16 and works

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,8 @@
--- 
+-- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 
-
+SELECT s.title, g.name FROM tv_genres AS g
+	INNER JOIN tv_show_genres AS s_g
+		ON g.id = s_g.genre_id
+	RIGHT JOIN tv_shows AS s
+		ON s_g.show_id = s.id
+	ORDER BY s.title ASC, g.name ASC;
