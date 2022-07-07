b359a2d96241620984e20537fe9544ec0e3231ed
WardenCode <diegojeanluck@hotmail.com>
2022-07-06 20:16:48 -0500

    Feat: advanced task #103 and works

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,3 +1,11 @@
--- 
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 
-
+SELECT g.name, SUM(s_r.rate) AS rating FROM tv_shows AS s
+	INNER JOIN tv_show_ratings AS s_r
+		ON s.id = s_r.show_id
+	INNER JOIN tv_show_genres AS s_g
+		ON s.id = s_g.show_id
+	INNER JOIN tv_genres AS g
+		ON g.id = s_g.genre_id
+	GROUP BY g.name
+	ORDER BY rating DESC;
