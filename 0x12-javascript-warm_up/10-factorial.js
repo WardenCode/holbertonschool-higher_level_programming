f55a9441ae3a8ca0a7ca5e4e122bedebc222dbd5
WardenCode <diegojeanluck@hotmail.com>
2022-08-01 19:05:42 -0500

    Feat: mandatory task #10 and works

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+
+const factorial = (num) => {
+  const casted = parseInt(num);
+  if (casted <= 1 || Number.isNaN(casted)) return (1);
+
+  return (factorial(num - 1) * casted);
+};
+
+console.log(factorial(argv[2]));
