a4965507b9eeb738e2c7ecd2ba2b8fb63e0e29db
WardenCode <diegojeanluck@hotmail.com>
2022-08-06 08:45:48 -0500

    Fix: using format instead Fstring

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
         print('Nothing')
     else:
         for row in states:
-            print(f'{row.id}: {row.name}')
+            print('{:d}: {:s}'.format(row.id, row.name))
     session.close()
