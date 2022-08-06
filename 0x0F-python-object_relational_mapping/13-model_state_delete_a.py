bee38116321ef7d8ad6fbda0eb2033fb50eba3a6
WardenCode <diegojeanluck@hotmail.com>
2022-08-06 08:39:12 -0500

    Eliminated validation of params (length)

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,23 +5,16 @@ from the database hbtn_0e_6_usa
 """
 
 
-from ast import Delete
-
-
 if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from sqlalchemy import create_engine
     from model_state import Base, State
     from sys import argv
 
-    if (len(argv) != 4):
-        print('Use: username, password database_name')
-        exit(1)
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
