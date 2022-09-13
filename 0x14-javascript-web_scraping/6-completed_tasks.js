dd5616609d004996e8405ecc30b5dbf010a58646
Diego Linares <diegojeanluck@hotmail.com>
2022-09-13 16:06:59 -0500

    Fix: Using for_each instead for_of

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,10 +5,9 @@ const baseUrl = process.argv[2];
 get(`${baseUrl}?completed=true`)
   .then(({ data }) => {
     const res = {};
-    for (const info of data) {
-      const userId = info.userId;
+    data.forEach(({ info: { userId } }) => {
       res[userId] !== undefined ? res[userId]++ : res[userId] = 1;
-    }
+    });
     console.log(res);
   })
   .catch((err) => console.error(err));
