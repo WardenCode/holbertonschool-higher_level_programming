dc66ccaac339376c6ca24c03ab5c800f85d76693
Diego Linares <diegojeanluck@hotmail.com>
2022-09-13 16:30:39 -0500

    Fix: now validating first the complete

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,8 +6,9 @@ get(`${baseUrl}`)
   .then(({ data }) => {
     const res = {};
     data.forEach(({ userId, completed }) => {
-      if (res[userId] === undefined) res[userId] = 0;
-      if (completed) res[userId] += 1;
+      if (completed) {
+        res[userId] === undefined ? res[userId] = 1 : res[userId]++;
+      }
     });
     console.log(res);
   })
