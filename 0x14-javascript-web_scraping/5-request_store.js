8d3b81fc3641f2ff98cb00691a8f0e8110b6f486
Diego Linares <diegojeanluck@hotmail.com>
2022-09-13 15:16:25 -0500

    Fix: using normal fs instead promise fs

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const { writeFile } = require('fs').promises;
+const { writeFile } = require('fs');
 const { get } = require('axios').default;
 const [,, url, path] = process.argv;
 
 get(url)
-  .then(({ data }) => writeFile(path, data, 'utf8'))
+  .then(({ data }) => writeFile(path, data, 'utf8', (err) => {
+    if (err) console.error(err);
+  }))
   .catch(err => console.error(err));
