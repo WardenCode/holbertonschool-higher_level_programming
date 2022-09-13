1a732b2a39c350aa10205109164c57b3eb563981
Diego Linares <diegojeanluck@hotmail.com>
2022-09-13 14:24:08 -0500

    Feat: mandatory task_4 and works

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { get } = require('axios').default;
+const urlSearch = 'https://swapi-api.hbtn.io/api/people/18/';
+
+get(process.argv[2])
+  .then(({ data }) => data)
+  .then(({ results }) => {
+    let count = 0;
+    for (const film of results) {
+      if (film.characters.some(url => url === urlSearch)) { count++; }
+    }
+    console.log(count);
+  })
+  .catch(err => console.error(err));
